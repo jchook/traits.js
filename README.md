db5ad616de1dc78a28771089d92f62921c2bac72
Wes Roberts <wes@sdynamics.com>
2014-04-25 02:06:31 -0400

    Allows for traits to be instantiated as self-sufficient objects (with proper conventions)

--- README.md
+++ README.md
@@ -3,25 +3,27 @@ traits.js
 
 ## What does traits.js do?
 
-Traits.js makes it possible for you to have horizontal inheritance pattern in your class structures.
+Traits.js makes it possible for you to have multiple inheritance pattern in your class structures.
 
-With traits, you may continue to use the vertical `this is a that` pattern, but you are no longer constrained to it. You may add as many traits as you like to an object, which truly widens the possibilities for producing simply [DRY](http://en.wikipedia.org/wiki/Don't_repeat_yourself) codebases.
+With traits, you may continue to use the standard `all poodles are dogs` pattern, 
+but you are no longer constrained to it. Now you can safely manage a modular inheritence network, 
+which truly widens the possibilities for producing simple, [DRY](http://en.wikipedia.org/wiki/Don't_repeat_yourself) codebases.
 
 ## Why use traits.js instead of xyz?
 
 * 42 lines of javascript, unminified
 * Fully-featured and to-specification
-* Use *any* javascript object as a Trait
+* Use any javascript object as a Trait
+* Simple. Convention over configuration
 
 ## What are some other features of traits.js?
 
 * Be notified of name conflicts
-* Resolve name conflicts explicitly
+* Resolve name conflicts expressly
 * Understand the code in less than 10 minutes
-* Require classes to implement specific methods
-* Nest traits within other traits for good ol' vertical inheriance
-* Control visibility of trait methods (public or private) 
-* Optionally influence instance state w/ small plugin
+* Require implementations to define specific properties
+* Nest traits within other traits for good ol' class-style inheriance
+* Allow traits to configure instance state via an explicit constructor callback
 
 ## Show me some examples!
 
--- traits.js
+++ traits.js
@@ -1,42 +1,42 @@
 // traits.js
 // by Wes Roberts <me@jchook.com>
-//
 ;(function(exports){
 
 	var extend = function(target, more, override) {
 		if (target && more) for (var i in more)
-			if (more.hasOwnProperty(i) && (i !== 'prototype'))
-				if ((typeof target[i] === 'undefined') || (more[i] !== Trait.required))
-					target[i] = (override || (typeof target[i] === 'undefined')) ? more[i] : Trait.conflict;
+			if (more.hasOwnProperty(i) && (i !== 'prototype') && (i !== '__traits__'))
+				if ((typeof target[i] === 'undefined') || (more[i] !== Traits.required))
+					target[i] = (override || (typeof target[i] === 'undefined')) ? more[i] : Traits.conflict;
 		return target;
 	};
 
-	var validate = function(obj) {
-		if (obj) for (var i in obj) 
-			if (obj.hasOwnProperty(i) && (i !== 'prototype')) 
-				if ((obj[i] == Trait.conflict) || (obj[i] == Trait.required))
-					obj[i].call(obj, i);
+	var validate = function() {
+		for (var x=0, l=arguments.length; x<l; x++) 
+			for (var y in arguments[x]) 
+				if ((arguments[x][y] == Traits.conflict) || (arguments[x][y] == Traits.required))
+						arguments[x][y].call(arguments[x], y);
 	};
 
-	var Trait = extend(function(){}, {
+	var Traits = extend(function(){ Traits.init(this, arguments); }, {
+		init: function(obj, args, traits) {
+			for (var i=0, l=(traits = (traits || obj.constructor.__traits__ || [])).length; i<l; i++) {
+				traits[i].apply(this, arguments);
+			}
+		},
 		compose: function (obj, traits, resolution) {
-			(obj.traits || (obj.traits = [])).push.apply(obj.traits, traits);
-			for (var i=0, l=traits.length; i<l; i++) {
-				extend(obj, traits[i]);
-				extend(obj.prototype, traits[i].prototype);
-			}
-			if (resolution) {
-				extend(obj, resolution, true);
-				extend(obj.prototype, resolution.prototype || {}, true);
-			}
-			validate(obj)
-			validate(obj.prototype);
+			obj.__traits__ || Object.defineProperty(obj, '__traits__', {value: []});
+			Array.prototype.push.apply(obj.__traits__, traits);
+			for (var i=0, l=traits.length; i<l; i++)
+				extend(extend(obj, traits[i]).prototype, traits[i].prototype);
+			if (resolution)
+				extend(extend(obj, resolution, true).prototype, resolution.prototype, true);
+			validate(obj, obj.prototype);
 			return obj;
 		},
 		conflict: function(prop, obj){ throw new Error('Conflicting property name: ' + prop); },
 		required: function(prop, obj){ throw new Error('Missing required property: ' + prop); }
 	});
  
-	exports.Trait = Trait;
+	exports.Traits = Traits;
 
 })(exports || this);
